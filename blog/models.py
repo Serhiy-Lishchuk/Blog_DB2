fa80f3381e0c7d2d5b87157e55ca52be127ccc8c
Serhiy <mcseruy@gmail.com>
2017-06-06 18:49:51 +0300

    add Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    content = models.ImageField()
+    likes = models.IntegerField(default=0)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'publication'
+
+    def __str__(self):
+        return self.title
+
+    def as_dict(self):
+        items = {
+            'author': self.author,
+            'title': self.title,
+            'text': self.text,
+            'content': self.content,
+            'likes': self.likes,
+            'created_date': self.created_date
+        }
+        return items
